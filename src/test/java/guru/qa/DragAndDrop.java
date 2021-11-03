08a79ccb5edca7dadcde1bb3f3a5403b3adbd45f
Алексей <vassilyevalexey@gmail.com>
2021-11-03 15:10:54 +0600

    Add dragAndDrop

---
+++ src/test/java/guru/qa/DragAndDrop.java
@@ -0,0 +1,45 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+
+        @BeforeAll
+        static void config() {
+            Configuration.startMaximized = true;
+            //Configuration.headless = true; не открывает страницу браузера
+        }
+
+        @Test
+        void dragAndDrop() {
+
+            // Открываем браузер
+            open("https://the-internet.herokuapp.com/drag_and_drop");
+
+            String squareA = "#column-a";
+            String squareB = "#column-b";
+
+            if($(squareA).$(byText("A")).is(Condition.visible)){
+                // Drag and Drop
+                $(squareA).dragAndDropTo(squareB);
+                //Проверяем
+                $(squareA).parent().shouldHave(Condition.text("B"));
+                // Вывод в лог
+                System.out.println("\nSuccessful movement of " + squareA + "\n");
+            }
+            else {
+                $(squareB).dragAndDropTo(squareA);
+                $(squareA).parent().shouldHave(Condition.text("B"));
+            }
+
+
+            sleep(5000);
+        }
+    }
+
--- src/test/java/guru/qa/HomeWork4.java
+++ src/test/java/guru/qa/HomeWork4.java
@@ -62,8 +62,4 @@ public class HomeWork4 {
         //sleep(5000);
     }
 
-
-
-
-
 }
